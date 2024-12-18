2beaa399069847c67d71449f0fac637a63de0b7d
RosaDelaVega <rosnicky19@gmail.com>
2024-12-18 23:13:39 +0100

    fix(bases): add product and division operations in arithmetic_operators.py

--- bases/arithmetic_operators.py
+++ bases/arithmetic_operators.py
@@ -1,7 +1,12 @@
 if __name__ == '__main__':
     a = int(input())
     b = int(input())
-    
+    # sum and diff
     print(a+b)
     print(a-b)
+    print(a*b)
+    # division
+    print(a//b)
+    print(a/b)
+    # product
     print(a*b)
