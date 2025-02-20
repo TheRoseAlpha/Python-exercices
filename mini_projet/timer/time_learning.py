06b445608f874a291524053eaa184a76d8536595
guede rosalba georgette karen <rosalba.guede@he-arc.ch>
2025-02-20 21:31:54 +0100

    refactor(projet) : rename projet to mini_projet

---
+++ mini_projet/timer/time_learning.py
@@ -0,0 +1,15 @@
+import time as tm
+
+
+# The time() function returns the number of seconds that have passed since the epoch.
+# seconds = tm.time()
+# print(seconds)
+
+# # print the curent time and date
+# print(tm.ctime(seconds)) #Thu Feb 20 20:52:31 2025
+
+# The time.sleep() Function creates a delay in a program that we can use to count time
+print("start of this program.")
+tm.sleep(5)
+print("end of the program")
+
