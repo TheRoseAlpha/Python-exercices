9526d89eff0c7b0b8d3d7bea083714a84a24842c
RosaDelaVega <rosnicky19@gmail.com>
2025-02-26 15:34:17 +0100

    feat(bases) : edit findtherunnerupscore.pa

--- bases/findTheRunnerUpScore.py
+++ bases/findTheRunnerUpScore.py
@@ -1,20 +1,22 @@
 if __name__ == '__main__':
     # my_list = [2, 3, 6, 6, 5]
     my_list = [57, 57, -57, 57]
+    # my_list = list(set(my_list))
+
     max_1 = 0
-    for i in range(1, len(my_list)-1):
+    for i in range(1, len(my_list)):
         if my_list[i] > my_list[max_1]:
             max_1 = i
-        elif my_list[i] == my_list[max_1]:
-            my_list.pop(max_1)
-        print(my_list[i])
-        
-    my_list.pop(max_1)
-        
-    max2_ = 0
-    for j in range(1, len(my_list)):
-        if my_list[j] > my_list[max2_]:
-            max2_ = j
-    print(my_list[max2_])   
+    if len(my_list) <= 2:
+        print(my_list[max_1])
+    elif len(my_list) > 2:
+        my_list.pop(max_1)
+        # for i in my_list:
+        #     print(i)
+        max2_ = 0
+        for j in range(1, len(my_list)):
+            if my_list[j] > my_list[max2_]:
+                max2_ = j
+        print(my_list[max2_])   
 
-    my_list = [57, 57, -57, 57]
+    # my_list = [57, 57, -57, 57]
