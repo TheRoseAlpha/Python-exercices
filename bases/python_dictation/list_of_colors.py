fc6189c87a05a8745f094263fa2044674b69b53d
RosaDelaVega <rosnicky19@gmail.com>
2025-02-26 19:16:35 +0100

    feat(bases): list_of_color in python dictation

---
+++ bases/python_dictation/list_of_colors.py
@@ -0,0 +1,10 @@
+colors = ["green", "red"]
+colors.append("purple")
+colors.append("blue")
+
+print(f"Initials colors: {colors[:-2]}")
+print(f"Initials colors: {["green", "red"]}")
+print(f"Uptated colors: {colors}")
+
+color_count = len(colors)
+print(f"The total number of colors: {color_count}")
