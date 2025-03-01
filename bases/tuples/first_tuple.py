e4aa71e2c2199361c848577e1c848576730c39ed
RosaDelaVega <rosnicky19@gmail.com>
2025-03-02 00:19:16 +0100

    fix(bases): edit first_tuple

--- bases/tuples/first_tuple.py
+++ bases/tuples/first_tuple.py
@@ -1,14 +1,11 @@
-if __name__ == '__main__':
-    n = int(input())
-    while True:
-        integer_list = map(int, input().split())
-        try:
-            if (len(integer_list)  > n) or (len(integer_list) < n):
-                raise ValueError
-            else:
-                break
-        except ValueError:
-            print("Non!")
+# Works only on python2
+
+# if __name__ == '__main__':
+#     n = int(raw_input())
+#     user_input = raw_input()
+#     list_of_numbers = [int(i) for i in user_input.split()]
+#     t = tuple(list_of_numbers)
+#     print(hash(t))  
     
-    t = tuple(integer_list)
-    print(hash(t))
+#     t = tuple(integer_list)
+#     print(hash(t))
