0899efcccc57468f62b317b8588ef95802956022
RosaDelaVega <rosnicky19@gmail.com>
2025-03-21 23:44:52 +0100

    feat(mini_projet): add book_contact.py

---
+++ mini_projet/book_contact/book_contact.py
@@ -0,0 +1,25 @@
+contact_dic = {}
+
+name = input("Name: ")
+number = input("Number: ")
+contact_dic.update({name: number})
+
+
+file_location = ".\\mini_projet\\book_contact\\contact_file.txt"
+file_contact = open(file_location, "a")
+
+
+for names, numbers in contact_dic.items():
+    file_contact.write(f"{names} : {numbers}\n")
+
+
+file_contact.close()
+file_contact = open(file_location, "r")
+
+
+for line in file_contact:
+    # line = file_contact.readline()
+    print(line)
+
+
+file_contact.close()
