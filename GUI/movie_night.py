e8e76e84e007acfa9c26c3ae30c4eacb6dc8f6c4
RosaDelaVega <rosnicky19@gmail.com>
2025-04-18 09:39:51 +0200

    feat(GUI): edit movie_night.py

--- GUI/movie_night.py
+++ GUI/movie_night.py
@@ -15,6 +15,8 @@ movies = { "Action": ["Mad Max: Fury Road", "John Wick", "Die Hard", "Gladiator"
             "Sci-Fi": ["Interstellar", "Inception", "The Matrix", "Blade Runner 2049"], 
             "Horror": ["The Conjuring", "A Nightmare on Elm Street", "Get Out", "The Exorcist"] 
         }
+
+
 genre = list(movies.keys())
 
 # month_cb.bind('<<ComboboxSelected>>', month_changed)
